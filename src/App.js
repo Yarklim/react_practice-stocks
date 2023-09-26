831ece8574f538e3e74819183dd857284937487a
Yaroslav Klimas <yarklim@gmail.com>
2023-09-26 18:12:21 +0300

    added useOnlineStatus

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Header from './components/Header/Header';
 import Router from './routes/Router';
+import NoInternet from './components/NoInternet/NoInternet';
+import { useOnlineStatus } from './hooks/useOnlineStatus';
 
 function App() {
+  const isOnline = useOnlineStatus();
+
+  if (!isOnline) return <NoInternet />;
+
   return (
-    <div className="App">
+    <div className="app">
       <Header />
       <Router />
     </div>
---
+++ src/components/NoInternet/NoInternet.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const NoInternet = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 18,
+        fontWeight: 500,
+      }}
+    >
+      ...No Internet connection
+    </div>
+  );
+};
+
+export default NoInternet;
